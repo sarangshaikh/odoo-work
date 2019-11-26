5a6b04bc6e0d18964ec64f931b2b681f3ef6509f
ayeshaashfaque <aashfaque@comstarusa.com>
2019-11-26 20:33:30 +0500

    removed custom module

--- meetings_from_oppurtunity/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import oppourtunity_view_ext
-
-
--- meetings_from_oppurtunity/models/oppourtunity_view_ext.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import _, api, fields, models
-
-
-class CrmLeadExtMeetings(models.Model):
-    _inherit = "crm.lead"
